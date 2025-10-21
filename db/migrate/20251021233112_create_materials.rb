85f04945cb361cce0f246ca3187626d2d9f6ba1a
Mariana Melo dos Santos <mariana@BOOK-II5BVKR4IV>
2025-10-21 20:43:36 -0300

    models: gerando variaveis especificas para cada tipo de material

--- db/migrate/20251021233112_create_materials.rb
+++ db/migrate/20251021233112_create_materials.rb
@@ -1,16 +1,12 @@
-class CreateMaterials < ActiveRecord::Migration[7.1]
+class AddSpecificFieldsToMaterials < ActiveRecord::Migration[7.1]
   def change
-    create_table :materials do |t|
-      t.string  :type, null: false           # tipo do material (Book | Article | Video)
-      t.string  :title, null: false
-      t.text    :description
-      t.integer :status, null: false, default: 0  # tipo de status do material (draft, published, archived)
-      t.references :author,  null: false, foreign_key: true
-      t.references :creator, null: false, foreign_key: { to_table: :users }
-      t.timestamps
-    end
-    add_index :materials, :type
-    add_index :materials, :status
-    add_index :materials, :title
+    add_column :materials, :isbn,             :string   # variavel para identificar materiais do tipo Book
+    add_column :materials, :page_count,       :integer  # armazena quantidade de paginas de Book
+    add_column :materials, :doi,              :string   # identificador do artigo 
+    add_column :materials, :duration_minutes, :integer  # duração do Video
+
+    # Unicidade por tipo de identificador
+    add_index :materials, :isbn, unique: true, where: "isbn IS NOT NULL"
+    add_index :materials, :doi,  unique: true, where: "doi IS NOT NULL"
   end
 end
