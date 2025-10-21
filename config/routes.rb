47d7c8df982ad846d49d357989c77a82326655d3
Mariana Melo dos Santos <mariana@BOOK-II5BVKR4IV>
2025-10-21 14:14:54 -0300

    routes: definir rotas do swagger, usuario, teste jwt

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,24 @@
 Rails.application.routes.draw do
-  mount Rswag::Ui::Engine => '/api-docs'
+  # Swagger (documentação)
   mount Rswag::Api::Engine => '/api-docs'
-  post "/graphql", to: "graphql#execute"
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  mount Rswag::Ui::Engine  => '/docs'
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  # GraphQL
+  post "/graphql", to: "graphql#execute"
+
+  # Autenticação Devise (JSON)
+  devise_for :users, defaults: { format: :json }
+
+  # Health check
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Namespace da API 
+  namespace :api do
+    namespace :v1 do
+      get "ping", to: "ping#index"
+    end
+  end
+
+  # root 
+  # root "api/v1/ping#index"
 end
