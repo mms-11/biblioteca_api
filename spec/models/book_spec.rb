e17c0617da7fc12acd637b3fa015d9868a6711a2
Mariana Melo dos Santos <mariana@BOOK-II5BVKR4IV>
2025-10-22 10:36:44 -0300

    specs: criando spec book para testes

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,11 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe Material, type: :model do
-  it { is_expected.to validate_presence_of(:type) }
-  it { is_expected.to validate_presence_of(:title) }
-  it { is_expected.to validate_length_of(:title).is_at_least(3).is_at_most(100) }
-  it { is_expected.to validate_length_of(:description).is_at_most(100) } rescue nil
-  it { is_expected.to validate_presence_of(:status) }
-  it { is_expected.to belong_to(:author) }
-  it { is_expected.to belong_to(:creator).class_name('User') }
+RSpec.describe Book, type: :model do
+  it { is_expected.to validate_presence_of(:isbn) }
+  it { is_expected.to validate_uniqueness_of(:isbn) }
+  it { is_expected.to allow_value('9781234567890').for(:isbn) }
+  it { is_expected.to validate_numericality_of(:page_count).only_integer.is_greater_than(0) }
 end
