cea5ca8c9da3e4e77167013457fb6195d1113519
Mariana Melo dos Santos <mariana@BOOK-II5BVKR4IV>
2025-10-22 09:32:23 -0300

    controllers: criando controller geral (base)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::API #classe base que todos os controlllers vao herdar!!
+  include Pundit::Authorization
+  before_action :authenticate_user!
+
+  rescue_from Pundit::NotAuthorizedError do #verifica autorizaçao do usuario e retorna http 403 se for proibibido o aceesso
+    render json: { error: 'forbidden' }, status: :forbidden
+  end
+
+  rescue_from ActiveRecord::RecordNotFound do #captura execeçoes
+    render json: { error: 'not_found' }, status: :not_found
+  end
+
+  rescue_from ActiveRecord::RecordInvalid do |e|
+    render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
+  end
 end
