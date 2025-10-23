0f255ffc67ef694e01f9c02e1e9c0c1a8db86fe8
Mariana Melo dos Santos <mariana@BOOK-II5BVKR4IV>
2025-10-23 08:29:11 -0300

    ajustando database.yml para conectar corretamente com orender

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -313,7 +313,7 @@ Devise.setup do |config|
   # config.sign_in_after_change_password = true
   # ==> Configuration for JWT
   config.jwt do |jwt|
-    jwt.secret = ENV['DEVISE_JWT_SECRET_KEY'] || Rails.application.credentials.devise_jwt_secret_key || Rails.application.secret_key_base
+    #jwt.secret = ENV['DEVISE_JWT_SECRET_KEY'] || Rails.application.credentials.devise_jwt_secret_key || Rails.application.secret_key_base
     
     jwt.dispatch_requests = [
       ['POST', %r{^/users/sign_in$}]
